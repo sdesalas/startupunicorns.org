520bc662dfaa88f9ac5d4b66482a9d508603857a
Steven de Salas <steven@desalasworks.com>
2023-08-14 04:27:13 +0100

    Latest updates

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,7 +1,7 @@
 
 
 
-const targetDate = new Date("2023-03-26T18:00:00.000Z");
+const targetDate = new Date("2023-09-15T18:00:00.000Z");
 class CountdownTimer {
   // setup timer values
   constructor({ selector, targetDate, backgroundColor = null, foregroundColor = null }) {

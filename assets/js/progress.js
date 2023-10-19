42b1532a770a845ecd4fad5277ad7001f5132115
Steven de Salas <steven@desalasworks.com>
2023-10-19 11:40:28 +0100

    Latest

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -5,10 +5,12 @@ var el = window.el = window.el || {
   progresscount: document.getElementById('progresscount'),
   progresstarget: document.getElementById('progresstarget'),
   progresstimeago: document.getElementById('progresstimeago'),
+  progressemail: document.getElementById('progressemail'),
+  progressemailcount: document.getElementById('progressemailcount'),
 };
-// Progress!
+// Progress Bar!
 (function () {
-  if (!el.progress) return;
+  if (!el.progress && !progressemailcount) return;
   const target = 1000;
   (async () => {
     const res = await fetch('https://startupunicorns-api.deno.dev/stats');
@@ -16,14 +18,21 @@ var el = window.el = window.el || {
     const count = stats && stats.member_count;
     const sub_date = stats && stats.last_sub_date;
     if (count) {
-      let percent = Math.ceil(count/target*100);
-      if (percent < 5) percent = 5;
-      console.log('We have a percent', percent);
-      el.progresscount.innerText = count;
-      el.progresstarget.innerText = target;
-      el.progresspercent.style.width = `${percent}%`;
-      el.progress.classList.toggle('displayed');
-      el.progresstimeago.innerText = timeago().format(sub_date);
+      console.log('We have a count', count);
+      if (el.progressemail) {
+        el.progressemailcount.innerText = count;
+        el.progressemail.classList.toggle('displayed')
+      }
+      if (el.progress) {
+        let percent = Math.ceil(count/target*100);
+        if (percent < 5) percent = 5;
+        console.log('We have a percent', percent);
+        el.progresscount.innerText = count;
+        el.progresstarget.innerText = target;
+        el.progresspercent.style.width = `${percent}%`;
+        el.progress.classList.toggle('displayed');
+        el.progresstimeago.innerText = timeago().format(sub_date);
+      }
     }
   })();
 })();

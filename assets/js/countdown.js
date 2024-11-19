84490e070b97fe1451283b728f6576236e1323be
Steven de Salas <steven@desalasworks.com>
2024-11-19 08:58:34 +0100

    Tuesday launch

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,7 +1,7 @@
 
 
 
-const targetDate = new Date("2024-12-01T14:00:00.000Z");
+const targetDate = new Date("2024-12-03T10:00:00.000Z");
 class CountdownTimer {
   // setup timer values
   constructor({ selector, targetDate, backgroundColor = null, foregroundColor = null }) {
